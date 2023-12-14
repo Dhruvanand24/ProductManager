8a6448147d6a8c8517b1691225ad5962a1e4dba5
Dhruv_Anand <ananddhruv475@gmail.com>
2023-12-14 17:39:06 +0530

    a

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measuremnetId: process.env.measurementId
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measuremnetId: import.meta.env.VITE_MEASUREMENTID
 };
 
 // Initialize Firebase
